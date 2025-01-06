b44038f33eaf12f5a8119d3b7618fa64a757e010
Richard Bentil <richardebo.bentil@gmail.com>
2025-01-06 23:32:10 +0000

    first change

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             <li>
               Get started by editing <code>pages/index.tsx</code>.
             </li>
-            <li>Save and see your changes instantly.</li>
+            <li>Save and see your changes.</li>
           </ol>
 
           <div className={styles.ctas}>
